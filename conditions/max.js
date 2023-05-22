9cb2cc560db9bd12097c0082b7642d2fc39165a5
Aziza Baccouri <azizabaccouri@gmail.com>
2023-05-22 14:02:10 +0200

    first commit

---
+++ conditions/max.js
@@ -0,0 +1,11 @@
+// Write an algorithm which given 3 numbers finds the *maximum*.
+
+function max(a,b,c){
+    let max=a;
+    if(a<b){
+        max=b;
+    }else if(a<c){
+        max=c;
+    }
+    
+}
